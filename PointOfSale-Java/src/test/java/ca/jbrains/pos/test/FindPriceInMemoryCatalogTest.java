003e7c4c9e010a1227aa9de7489345968371922d
J. B. Rainsberger <me@jbrains.ca>
2018-11-20 15:30:07 +0100

    We can describe findPrice() with a contract test, whether we find the barcode or not.

--- PointOfSale-Java/src/test/java/ca/jbrains/pos/test/FindPriceInMemoryCatalogTest.java
+++ PointOfSale-Java/src/test/java/ca/jbrains/pos/test/FindPriceInMemoryCatalogTest.java
@@ -23,8 +23,12 @@ public class FindPriceInMemoryCatalogTest {
 
     @Test
     public void productNotFound() throws Exception {
-        InMemoryCatalog catalog = new InMemoryCatalog(Collections.emptyMap());
+        Catalog catalog = emptyCatalog();
         Assert.assertEquals(null, catalog.findPrice("::missing barcode::"));
+    }
+
+    private Catalog emptyCatalog() {
+        return new InMemoryCatalog(Collections.emptyMap());
     }
 
     public static class InMemoryCatalog implements Catalog {
