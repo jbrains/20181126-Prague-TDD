fda184e61a54928a08601da3d97f6762d2bfcb77
J. B. Rainsberger <me@jbrains.ca>
2018-11-26 14:38:59 +0100

    We can now sell items of different prices.

--- PointOfSale-Java/src/test/java/ca/jbrains/pos/test/SellOneItemTest.java
+++ PointOfSale-Java/src/test/java/ca/jbrains/pos/test/SellOneItemTest.java
@@ -1,27 +1,51 @@
 package ca.jbrains.pos.test;
 
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class SellOneItemTest {
     @Test
     public void productFound() throws Exception {
         final Display display = new Display();
-        final Sale sale = new Sale();
+        final Sale sale = new Sale(display);
 
         sale.onBarcode("12345");
 
         Assert.assertEquals("CZK 780.00", display.getText());
     }
 
-    public static class Sale {
-        public void onBarcode(String barcode) {
+    @Ignore("refactoring")
+    @Test
+    public void anotherProductFound() throws Exception {
+        final Display display = new Display();
+        final Sale sale = new Sale(display);
 
+        sale.onBarcode("23456");
+
+        Assert.assertEquals("CZK 1249.00", display.getText());
+    }
+
+    public static class Sale {
+        private Display display;
+
+        public Sale(Display display) {
+            this.display = display;
+        }
+
+        public void onBarcode(String barcode) {
+            display.setText("CZK 780.00");
         }
     }
     public static class Display {
+        private String text;
+
         public String getText() {
-            return "CZK 780.00";
+            return text;
+        }
+
+        public void setText(String text) {
+            this.text = text;
         }
     }
 }
