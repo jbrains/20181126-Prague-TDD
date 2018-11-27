126767d737f9e241ab34c739a97a225ad33e60ff
J. B. Rainsberger <me@jbrains.ca>
2018-11-27 14:50:14 +0100

    We can now more-easily create a multiline string from a bunch of lines.

--- PointOfSale-Java/src/test/java/ca/jbrains/pos/test/StreamStdinAsLinesTest.java
+++ PointOfSale-Java/src/test/java/ca/jbrains/pos/test/StreamStdinAsLinesTest.java
@@ -1,5 +1,6 @@
 package ca.jbrains.pos.test;
 
+import io.vavr.collection.List;
 import io.vavr.collection.Stream;
 import org.junit.Assert;
 import org.junit.Test;
@@ -26,7 +27,7 @@ public class StreamStdinAsLinesTest {
     public void oneLineEndingInALineSeparator() throws Exception {
         Assert.assertEquals(
                 Stream.of("::line::"),
-                streamAsLines(new StringReader("::line::" + System.lineSeparator())));
+                streamAsLines(new StringReader(toMultilineText(List.of("::line::")))));
     }
 
     @Test
@@ -43,5 +44,14 @@ public class StreamStdinAsLinesTest {
         } catch (IOException e) {
             throw new RuntimeException("Reading commands from a Reader failed somehow.", e);
         }
+    }
+
+    // CONTRACT Adds a trailing line separator to the entire "document".
+    // REFACTOR Move me to some extension class for StringBuilder. Maybe DocumentBuilder?
+    private static String toMultilineText(final List<String> lines) {
+        return lines.foldLeft(
+                new StringBuilder(),
+                (sb, line) -> sb.append(line).append(System.lineSeparator())
+        ).toString();
     }
 }
