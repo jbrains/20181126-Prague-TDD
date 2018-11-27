4a883eb4ba5bd0db9e1c1bc1d798cd26c94c329f
J. B. Rainsberger <me@jbrains.ca>
2018-11-27 14:43:48 +0100

    We can stream text as lines, as long as there are no line separators.

--- PointOfSale-Java/src/test/java/ca/jbrains/pos/test/StreamStdinAsLinesTest.java
+++ PointOfSale-Java/src/test/java/ca/jbrains/pos/test/StreamStdinAsLinesTest.java
@@ -4,6 +4,8 @@ import io.vavr.collection.Stream;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.Reader;
 import java.io.StringReader;
 
@@ -13,7 +15,19 @@ public class StreamStdinAsLinesTest {
         Assert.assertEquals(Stream.empty(), streamAsLines(new StringReader("")));
     }
 
+    @Test
+    public void oneLineWithoutALineSeparator() throws Exception {
+        Assert.assertEquals(
+                Stream.of("::line::"),
+                streamAsLines(new StringReader("::line::")));
+    }
+
     private Stream<String> streamAsLines(Reader source) {
-        return Stream.empty();
+        try {
+            final String line = new BufferedReader(source).readLine();
+            return line == null ? Stream.empty() : Stream.of(line);
+        } catch (IOException e) {
+            throw new RuntimeException("Reading commands from a Reader failed somehow.", e);
+        }
     }
 }
